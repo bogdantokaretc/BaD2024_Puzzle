9a1850c2d8ef38861da13c0dcefb468758dec9e9
Bohdan Local <tokaretcbogdan@gmail.com>
2024-12-12 00:43:26 +0200

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace BaD2024_Puzzle
+{
+    static class Menu
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
+            {
+                Console.WriteLine("\n\tМеню для роботи з текстовими файлами:");
+                Console.WriteLine("1. Вибрати .txt файл з поточної папки програми");
+                Console.WriteLine("2. Вийти");
+                Console.WriteLine($"\nУВАГА! Для коректної роботи програми помістіть файл в поточну папку:\n{Directory.GetCurrentDirectory()}\n");
+                Console.Write("Введіть свій вибір: ");
+
+                string? choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        ListFilesInDirectory();
+                        break;
+                    case "2":
+                        Console.WriteLine("Завершення роботи програми...");
+                        return;
+                    default:
+                        Console.WriteLine("Даного пункту меню не існує!");
+                        break;
+                }
+            }
+        }
+
+        static void ListFilesInDirectory()
+        {
+            string filePath = Directory.GetCurrentDirectory();
+
+            string[] txtFiles = Directory.GetFiles(filePath, "*.txt");
+
+            if (txtFiles.Length == 0)
+            {
+                Console.WriteLine("У поточній директорії файл з розширенням (*.txt) не знайдено!");
+                return;
+            }
+
+            Console.WriteLine("\nУ папці знайдено такі файли з розширенням (*.txt):");
+            for (int i = 0; i < txtFiles.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Path.GetFileName(txtFiles[i])}");
+            }
+
+            Console.Write("Введіть номер файлу з списку для його подальшого опрацювання\n" +
+                "Для повернення до головного меню натисність Enter на клавіатурі:");
+
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int index) && index > 0 && index <= txtFiles.Length)
+            {
+                ProcessFile(txtFiles[index - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Даного пункту меню не існує!");
+            }
+        }
+
+        static void ProcessFile(string filePath)
+        {
+            Console.WriteLine($"\nВідкрито файл: {filePath}");
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine("\nВміст файлу:");
+                Console.WriteLine(content);
+                Console.WriteLine("\nРезультат роботи програми:");
+                Program.GetFragmentChain(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Сталася помилка під час обробки файлу: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace BaD2024_Puzzle
+{
+    internal static class Program
+    {
+        internal static void GetFragmentChain(string filePath) 
+        {
+            var fragments = File.ReadAllLines(filePath).Where(line => !string.IsNullOrWhiteSpace(line)).ToList();
+
+            // Змінні для збереження результату
+            StringBuilder bestSequence = new();
+            List<string> bestChain = [];
+
+            //Визначення стартового фрагменту послідовності
+            var startFragment = fragments[0];
+            fragments.RemoveAt(0);
+            List<string> remainingFragments = new(fragments);
+
+            //Пошук ланцюга
+            FindLongestChain(remainingFragments, [startFragment]);
+
+            // Формування послідовності
+            for (int i = 0; i < bestChain.Count; i++)
+            {
+                if (i != 0) bestSequence.Append(bestChain[i][2..]);
+                else bestSequence.Append(bestChain[i]);
+            }
+            //Вивід результатів
+            Console.WriteLine("Найдовша послідовність:");
+            Console.WriteLine(bestSequence);
+            Console.WriteLine("Ланцюжок фрагментів:");
+            Console.WriteLine(string.Join(" -> ", bestChain));
+            Console.ReadKey();
+
+
+
+            List<string> FindLongestChain(List<string> remainingFragments, List<string> currentChain)
+            {
+                if (remainingFragments.Count == 0)
+                {
+                    bestChain.AddRange(currentChain);
+                    return bestChain;
+                }
+                else
+                {
+                    for (int i = 0; i < remainingFragments.Count; i++)
+                    {
+                        //Якщо останні дві цифри ланцюга
+                        if (remainingFragments[i].StartsWith(currentChain[^1][^2..]))
+                        {
+                            currentChain.Add(remainingFragments[i]);
+                            fragments.Remove(remainingFragments[i]);
+                        }
+                        //Якщо перші дві цифри ланцюга
+                        else if (currentChain[0].StartsWith(remainingFragments[i][^2..]))
+                        {
+                            currentChain.Insert(0, remainingFragments[i]);
+                            fragments.Remove(remainingFragments[i]);
+                        }
+                    }
+                    remainingFragments.Clear();
+                }
+                return FindLongestChain(fragments, currentChain);
+            }
+        }
+    }
+}
